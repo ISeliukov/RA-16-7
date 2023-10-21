0842c7f0d92dbaf4af9b724445ee2817bd044e2b
Garysi <garysi@rambler.ru>
2023-10-21 14:01:20 +0300

    add ver 0.2

--- ra_16_7/src/engine/Popular.js
+++ ra_16_7/src/engine/Popular.js
@@ -18,6 +18,36 @@ function Popular(props) {
     )
 };
 
+function withPopularOrNew(WrappedComponent) {
+  const WithPopularOrNew = (props) => {
+    const { views } = props;
+    let Wrapper = '';
+    if (views < 100) {
+      Wrapper = New;
+      return (
+        <Wrapper>
+          <WrappedComponent {...props} />
+        </Wrapper>
+      );	  
+    };
+	if (views >= 1000) {
+      Wrapper = Popular;
+      return (
+        <Wrapper>
+          <WrappedComponent {...props} />
+        </Wrapper>
+      );	  
+    };
+	return (
+        <WrappedComponent {...props} />
+    );
+  };
+  const displayName =
+  WrappedComponent.displayName || WrappedComponent.name | 'Component';
+  WithPopularOrNew.displayName = `WithPopularOrNew(${displayName})`;
+  return WithPopularOrNew;
+}
+
 function Article(props) {
     return (
         <div className="item item-article">
@@ -36,21 +66,19 @@ function Video(props) {
     )
 };
 
+const PopularOrNewArticle = withPopularOrNew(Article);
+const PopularOrNewVideo = withPopularOrNew(Video);
+
 function List(props) {
-    return props.list.map(item => {
-        switch (item.type) {
-            case 'video':
-                return (
-                    <Video {...item} />
-                );
+  return props.list.map((item) => {
+    switch (item.type) {
+      case 'video':
+        return <PopularOrNewVideo {...item} />;
 
-            case 'article':
-                return (
-                    <Article {...item} />
-                );
-        }
-    });
-};
+      case 'article':
+        return <PopularOrNewArticle {...item} />;
+    }
+  });
+}
 
-export { List }
-
+export { List };
